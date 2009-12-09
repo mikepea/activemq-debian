28a95fc4ed5a4ae5017c2fd2c6d387665ef57f54
Brecht Hoflack <brh@melexis.com>
2009-12-09 13:49:42 +0100

    Adapted the init script.

--- debian/init.d
+++ debian/init.d
@@ -18,84 +18,32 @@ set -e
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 DESC="Apache activemq daemon"
 NAME=apache-activemq
-DAEMON=/usr/share/apache-activemq/bin/activemq
-PIDFILE=/var/run/$NAME.pid
-SCRIPTNAME=/etc/init.d/$NAME
+ACTIVEMQADMIN=/usr/share/apache-activemq/bin/activemq-admin
+CMD="nohup $ACTIVEMQADMIN"
 
 # Gracefully exit if the package has been removed.
-test -x $DAEMON || exit 0
-
-# Read config file if it is present.
-#if [ -r /etc/default/$NAME ]
-#then
-#	. /etc/default/$NAME
-#fi
-
-#
-#	Function that starts the daemon/service.
-#
-d_start() {
-	start-stop-daemon --start --quiet --pidfile $PIDFILE \
-		--background --exec $DAEMON
-}
-
-#
-#	Function that stops the daemon/service.
-#
-d_stop() {
-	start-stop-daemon --stop --quiet --pidfile $PIDFILE \
-		--name $NAME
-}
-
-#
-#	Function that sends a SIGHUP to the daemon/service.
-#
-d_reload() {
-	start-stop-daemon --stop --quiet --pidfile $PIDFILE \
-		--name $NAME --signal 1
-}
+test -x $ACTIVEMQADMIN || exit 0
 
 case "$1" in
-  start)
-	echo -n "Starting $DESC: $NAME"
-	d_start
-	echo "."
-	;;
-  stop)
-	echo -n "Stopping $DESC: $NAME"
-	d_stop
-	echo "."
-	;;
-  #reload)
-	#
-	#	If the daemon can reload its configuration without
-	#	restarting (for example, when it is sent a SIGHUP),
-	#	then implement that here.
-	#
-	#	If the daemon responds to changes in its config file
-	#	directly anyway, make this an "exit 0".
-	#
-	# echo -n "Reloading $DESC configuration..."
-	# d_reload
-	# echo "done."
-  #;;
-  restart|force-reload)
-	#
-	#	If the "reload" option is implemented, move the "force-reload"
-	#	option to the "reload" entry above. If not, "force-reload" is
-	#	just the same as "restart".
-	#
-	echo -n "Restarting $DESC: $NAME"
-	d_stop
-	sleep 1
-	d_start
-	echo "."
-	;;
-  *)
-	# echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
-	exit 1
-	;;
-esac
+    start)
+        echo "Starting ActiveMQ "
+        /bin/su - $AMQ_USER -c "$CMD $1 >/dev/null 2>&1 &"
+        ;;
+    stop)
+        echo "Shutting down ActiveMQ"
+        /bin/su - $AMQ_USER -c "$CMD $1 >/dev/null 2>&1 &"
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    status)
+        echo not yet implemented
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|status}"
+        exit 1
+        ;;
+esac 
 
 exit 0
